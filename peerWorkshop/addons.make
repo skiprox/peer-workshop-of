372b623ec14dd2314282e452b92f6dc1a206bd35
skiprox <seanmichaelscanlan@gmail.com>
2020-03-26 12:58:33 -0400

    Adding in openframeworks project start

---
+++ peerWorkshop/addons.make
@@ -0,0 +1,3 @@
+ofxGui
+ofxTriangleMesh
+ofxXmlSettings
---
+++ peerWorkshop/src/ofApp.cpp
@@ -0,0 +1,38 @@
+#include "ofApp.h"
+
+float circleX = 0.0;
+
+//---------------------------------------
+void ofApp::setup(){
+	ofSetCircleResolution(100);
+	ofBackground(34, 34, 34);
+	// shader.load("shaders/start.vert","shaders/start.frag");
+}
+
+//---------------------------------------
+void ofApp::update(){
+	circleX = ofMap(
+		sin(ofGetElapsedTimef()),
+		-1.0,
+		1.0,
+		0.0,
+		ofGetWidth()
+	);
+}
+
+//---------------------------------------
+void ofApp::draw(){
+	ofSetColor(245, 58, 135);
+	ofFill();
+	// shader.begin();
+	ofDrawCircle(circleX,
+		ofGetHeight()/2.0,
+		100.0
+	);
+	// shader.end();
+}
+
+//---------------------------------
+// CODE CHUNKS
+// CHUNKY CODES
+//---------------------------------
